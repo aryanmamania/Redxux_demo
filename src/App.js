017172dc8a1a2eee68e08f5351afcbc423f6728c
mamania0217 <aryanmamania17@gmail.com>
2023-08-20 11:04:08 +0530

    redux_demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector , useDispatch } from 'react-redux';
+import {actions} from './store/index'
 
 function App() {
+  const counter = useSelector((state)=> state.counter);
+  const disptach = useDispatch();
+  const increment =()=>{
+disptach(actions.increment());
+  };
+  const decrement =()=>{
+    disptach(actions.decrement());
+  }
+  const addBy=()=>{
+    disptach(actions.addBy(10));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      <h1>Counter App</h1>
+      <h2>{counter}</h2>
+     <button onClick={increment}>increment</button>
+     <button onClick={decrement}>decrement</button> 
+     <button onClick={addBy}>Add value</button>
     </div>
   );
 }
